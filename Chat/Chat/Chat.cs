aec545a97f9a3bf6c990430b2937b517e1f43ce0
Drummermichal <drummermichal@gmail.com>
2023-02-03 13:19:27 +0100

    pa

---
+++ Chat/Chat/Chat.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat
+{
+    public partial class FormChat : Form
+    {
+        public string serverAdress;
+        public int port;
+
+        private IPHostEntry host;
+        private IPAddress ipAddress;
+        private IPEndPoint remoteEP;
+        private Socket socket;
+
+        private List<string> chat;
+
+        public FormChat()
+        {
+            InitializeComponent();
+        }
+
+        private void FormChat_Load(object sender, EventArgs e)
+        {
+            if(serverAdress != null && port != 0)
+            {
+                try
+                {
+                    host = Dns.GetHostEntry(serverAdress);
+                    ipAddress = host.AddressList[0];
+                    remoteEP = new IPEndPoint(ipAddress, port);
+
+                    socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                    try
+                    {
+                        socket.Connect(remoteEP);
+
+                        byte[] msg = Encoding.UTF8.GetBytes("Get users: " + Text);
+                        int bytesSent = socket.Send(msg);
+
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = socket.Receive(bytes);
+                        string response = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                        // socket.Shutdown(SocketShutdown.Both);
+                        // socket.Close();
+                    }
+                    catch (ArgumentNullException ex)
+                    {
+                        MessageBox.Show("ArgumentNullException : " + ex.Message);
+                    }
+                    catch (SocketException ex)
+                    {
+                        MessageBox.Show("SocketException : " + ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Unexpected exception : " + ex.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unexpected exception : " + ex.Message);
+                }
+            }
+            else
+            {
+                string response = "test user 1;test user 2;test user3";
+                string[] users = response.Split(';');
+                foreach(var user in users)
+                {
+                    Button button = new Button();
+                    button.Text = user;
+                    button.Size = new Size(222, 41);
+                    button.Click += new EventHandler(buttonUser_Click);
+
+                    flowLayoutPanel2.Controls.Add(button);
+                }
+            }
+        }
+
+        private void ReceiveMessage(string user)
+        {
+            if (host != null && remoteEP != null && socket != null)
+            {
+                try
+                {
+                    while (true)
+                    {
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = socket.Receive(bytes);
+                        string response = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                        chat = new List<string>();
+                        chat.AddRange(response.Split('\n'));
+                    }
+                }
+                catch (ArgumentNullException ex)
+                {
+                    MessageBox.Show("ArgumentNullException : " + ex.Message);
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show("SocketException : " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unexpected exception : " + ex.Message);
+                }
+            }
+            else
+            {
+                // Test
+                // Test
+            }
+        }
+
+        private void buttonUser_Click(object sender, EventArgs e)
+        {
+            label1.Text = ((Button)sender).Text;
+            richTextBox1.Text = "";
+            if (host != null && remoteEP != null && socket != null)
+            {
+                try
+                {
+                    byte[] msg = Encoding.UTF8.GetBytes("Get messages: " + Text + " " + ((Button)sender).Text);
+                    int bytesSent = socket.Send(msg);
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = socket.Receive(bytes);
+                    string response = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                    chat = new List<string>();
+                    chat.AddRange(response.Split('\n'));
+                }
+                catch (ArgumentNullException ex)
+                {
+                    MessageBox.Show("ArgumentNullException : " + ex.Message);
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show("SocketException : " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unexpected exception : " + ex.Message);
+                }
+            }
+            else
+            {
+                string response = "[01.02.2023 08:12:03] User1 -> XD\n[01.02.2023 08:12:03] User2 -> XD\n[01.02.2023 08:12:03] User1 -> XD2\n[01.02.2023 08:12:03] User1 -> XD3\n";
+                chat = new List<string>();
+                chat.AddRange(response.Split('\n'));
+
+                foreach(var msg in chat)
+                {
+                    richTextBox1.AppendText("\r\n" + msg);
+                }
+                richTextBox1.ScrollToCaret();
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            chat.Add(textBoxMessage.Text);
+            richTextBox1.AppendText("\r\n" + textBoxMessage.Text);
+            richTextBox1.ScrollToCaret();
+            if (host != null && remoteEP != null && socket != null)
+            {
+                try
+                {
+                    byte[] msg = Encoding.UTF8.GetBytes("Send message: " + Text + " " + label1.Text + " " + textBoxMessage.Text);
+                    int bytesSent = socket.Send(msg);
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = socket.Receive(bytes);
+                    string response = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                }
+                catch (ArgumentNullException ex)
+                {
+                    MessageBox.Show("ArgumentNullException : " + ex.Message);
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show("SocketException : " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Unexpected exception : " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Chat/Chat/SignIn.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Chat
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.buttonSignIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Location = new System.Drawing.Point(91, 140);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(190, 20);
+            this.textBoxAdress.TabIndex = 0;
+            this.textBoxAdress.Text = "127.0.0.1";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(91, 114);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(190, 20);
+            this.textBoxUsername.TabIndex = 1;
+            // 
+            // buttonSignIn
+            // 
+            this.buttonSignIn.Location = new System.Drawing.Point(109, 192);
+            this.buttonSignIn.Name = "buttonSignIn";
+            this.buttonSignIn.Size = new System.Drawing.Size(75, 23);
+            this.buttonSignIn.TabIndex = 2;
+            this.buttonSignIn.Text = "Sign In";
+            this.buttonSignIn.UseVisualStyleBackColor = true;
+            this.buttonSignIn.Click += new System.EventHandler(this.buttonSignIn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(84, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sign In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Server Adress:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Port:";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(91, 166);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(190, 20);
+            this.textBoxPort.TabIndex = 4;
+            this.textBoxPort.Text = "1000";
+            // 
+            // SignIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 253);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSignIn);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxAdress);
+            this.Name = "SignIn";
+            this.Text = "SignIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Button buttonSignIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPort;
+    }
+}
