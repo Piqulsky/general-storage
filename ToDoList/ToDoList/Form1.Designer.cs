b832f18f87ee6dbdb0a3902ebc54444ec0c7f1a1
Drummermichal <drummermichal@gmail.com>
2023-03-03 13:59:28 +0100

    pa

---
+++ ToDoList/ToDoList/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxNewTask = new System.Windows.Forms.TextBox();
+            this.toDoList = new System.Windows.Forms.ListBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonDelete, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.toDoList, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxNewTask, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonAdd, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(492, 563);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonAdd.Location = new System.Drawing.Point(347, 3);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(142, 106);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Dodaj";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxNewTask
+            // 
+            this.textBoxNewTask.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxNewTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxNewTask.Location = new System.Drawing.Point(3, 3);
+            this.textBoxNewTask.Name = "textBoxNewTask";
+            this.textBoxNewTask.Size = new System.Drawing.Size(338, 35);
+            this.textBoxNewTask.TabIndex = 1;
+            // 
+            // toDoList
+            // 
+            this.toDoList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toDoList.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.toDoList.FormattingEnabled = true;
+            this.toDoList.ItemHeight = 29;
+            this.toDoList.Location = new System.Drawing.Point(3, 115);
+            this.toDoList.Name = "toDoList";
+            this.toDoList.Size = new System.Drawing.Size(338, 445);
+            this.toDoList.TabIndex = 2;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonDelete.Location = new System.Drawing.Point(347, 115);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(142, 106);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Usuń";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 563);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "To Do List";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ListBox toDoList;
+        private System.Windows.Forms.TextBox textBoxNewTask;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
---
+++ ToDoList/ToDoList/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                FileStream fs = new FileStream("list.moscicki", FileMode.Open);
+                BinaryFormatter bf = new BinaryFormatter();
+                var itemsInFile = (object[])bf.Deserialize(fs);
+                toDoList.Items.AddRange(itemsInFile);
+                fs.Close();
+            }
+            catch {  }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            toDoList.Items.Add(textBoxNewTask.Text);
+
+            FileStream fs = new FileStream("list.moscicki", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            object[] listItems = new object[toDoList.Items.Count];
+            toDoList.Items.CopyTo(listItems, 0);
+            bf.Serialize(fs, listItems);
+            fs.Close();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            toDoList.Items.Remove(toDoList.SelectedItem);
+
+            FileStream fs = new FileStream("list.moscicki", FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            object[] listItems = new object[toDoList.Items.Count];
+            toDoList.Items.CopyTo(listItems, 0);
+            bf.Serialize(fs, listItems);
+            fs.Close();
+        }
+    }
+}
