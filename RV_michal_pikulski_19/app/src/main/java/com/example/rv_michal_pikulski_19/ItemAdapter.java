c31560be30466f7e4c1eea6064638583dcc7c5a5
Drummermichal <drummermichal@gmail.com>
2023-03-23 15:29:07 +0100

    Pam

---
+++ RV_michal_pikulski_19/app/src/main/java/com/example/rv_michal_pikulski_19/ItemAdapter.java
@@ -0,0 +1,35 @@
+package com.example.rv_michal_pikulski_19;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemHolder> {
+    public ItemAdapter(List<TransactionData> list){
+        dataStore = list;
+    }
+    private List<TransactionData> dataStore;
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_recycler_item, parent, false);
+        return new ItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        holder.getTextView().setText(dataStore.get(position).text);
+        holder.getSecondaryTextView().setText(dataStore.get(position).secondaryText);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataStore.size();
+    }
+}
---
+++ RV_michal_pikulski_19/app/src/main/java/com/example/rv_michal_pikulski_19/TransactionData.java
@@ -0,0 +1,10 @@
+package com.example.rv_michal_pikulski_19;
+
+public class TransactionData {
+    TransactionData(String text, String secondaryText){
+        this.text = text;
+        this.secondaryText = secondaryText;
+    }
+    public String text;
+    public String secondaryText;
+}
