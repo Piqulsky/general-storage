e7832ee3b3044f552a30fab2affac1618f16df05
Drummermichal <drummermichal@gmail.com>
2023-04-12 09:01:47 +0200

    pa

--- ToDoList/ToDoList/Form1.Designer.cs
+++ ToDoList/ToDoList/Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
@@ -35,8 +37,16 @@
             this.buttonDelete = new System.Windows.Forms.Button();
             this.toDoList = new System.Windows.Forms.ListBox();
             this.buttonAdd = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonAdd = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonDelete = new System.Windows.Forms.ToolStripButton();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -48,13 +58,12 @@
             this.tableLayoutPanel1.Controls.Add(this.buttonDelete, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.toDoList, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.buttonAdd, 1, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 28);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(492, 563);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(492, 510);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -69,14 +78,14 @@
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(338, 106);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(338, 96);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(3, 56);
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 51);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(332, 26);
             this.dateTimePicker1.TabIndex = 1;
@@ -95,7 +104,7 @@
             // 
             this.buttonDelete.Dock = System.Windows.Forms.DockStyle.Top;
             this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonDelete.Location = new System.Drawing.Point(347, 115);
+            this.buttonDelete.Location = new System.Drawing.Point(347, 105);
             this.buttonDelete.Name = "buttonDelete";
             this.buttonDelete.Size = new System.Drawing.Size(142, 106);
             this.buttonDelete.TabIndex = 3;
@@ -109,9 +118,9 @@
             this.toDoList.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.toDoList.FormattingEnabled = true;
             this.toDoList.ItemHeight = 29;
-            this.toDoList.Location = new System.Drawing.Point(3, 115);
+            this.toDoList.Location = new System.Drawing.Point(3, 105);
             this.toDoList.Name = "toDoList";
-            this.toDoList.Size = new System.Drawing.Size(338, 445);
+            this.toDoList.Size = new System.Drawing.Size(338, 402);
             this.toDoList.TabIndex = 2;
             // 
             // buttonAdd
@@ -120,24 +129,78 @@
             this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.buttonAdd.Location = new System.Drawing.Point(347, 3);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(142, 106);
+            this.buttonAdd.Size = new System.Drawing.Size(142, 96);
             this.buttonAdd.TabIndex = 0;
             this.buttonAdd.Text = "Dodaj";
             this.buttonAdd.UseVisualStyleBackColor = true;
             this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonAdd,
+            this.toolStripButtonDelete});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(492, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonAdd
+            // 
+            this.toolStripButtonAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonAdd.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAdd.Image")));
+            this.toolStripButtonAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAdd.Name = "toolStripButtonAdd";
+            this.toolStripButtonAdd.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonAdd.Text = "Dodaj";
+            this.toolStripButtonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // toolStripButtonDelete
+            // 
+            this.toolStripButtonDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonDelete.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonDelete.Image")));
+            this.toolStripButtonDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonDelete.Name = "toolStripButtonDelete";
+            this.toolStripButtonDelete.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonDelete.Text = "Usuń";
+            this.toolStripButtonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 541);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(492, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(450, 16);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(492, 563);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "To Do List";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -150,6 +213,12 @@
         private System.Windows.Forms.Button buttonAdd;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonAdd;
+        private System.Windows.Forms.ToolStripButton toolStripButtonDelete;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
     }
 }
 
--- ToDoList/ToDoList/Form1.cs
+++ ToDoList/ToDoList/Form1.cs
@@ -25,8 +25,17 @@ namespace ToDoList
                 completeList = (Dictionary<string, object[]>)bf.Deserialize(fs);
                 toDoList.Items.AddRange(completeList[dateTimePicker1.Value.ToShortDateString()]);
                 fs.Close();
+                toolStripProgressBar1.Value = 100;
             }
             catch {  }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(buttonAdd, "Dodaj do listy");
+            toolTip1.SetToolTip(buttonDelete, "Usuń z listy");
+            toolTip1.SetToolTip(textBoxNewTask, "Wpisz nowe zadanie");
+            toolTip1.SetToolTip(dateTimePicker1, "Podaj datę, którą chcesz otworzyć");
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
@@ -46,6 +55,8 @@ namespace ToDoList
             toDoList.Items.CopyTo(completeList[dateTimePicker1.Value.ToShortDateString()], 0);
             bf.Serialize(fs, completeList);
             fs.Close();
+            toolStripProgressBar1.Value = 0;
+            toolStripProgressBar1.Value = 100;
         }
 
         private void buttonDelete_Click(object sender, EventArgs e)
@@ -58,6 +69,8 @@ namespace ToDoList
             toDoList.Items.CopyTo(completeList[dateTimePicker1.Value.ToShortDateString()], 0);
             bf.Serialize(fs, completeList);
             fs.Close();
+            toolStripProgressBar1.Value = 0;
+            toolStripProgressBar1.Value = 100;
         }
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
@@ -66,6 +79,8 @@ namespace ToDoList
             {
                 toDoList.Items.Clear();
                 toDoList.Items.AddRange(completeList[dateTimePicker1.Value.ToShortDateString()]);
+                toolStripProgressBar1.Value = 0;
+                toolStripProgressBar1.Value = 100;
             }
             catch (Exception exc)
             {
